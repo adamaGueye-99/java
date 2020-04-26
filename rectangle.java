8ec510ab673d1f8f7225409c6ee0c1e4eaabf33b
adamaGueye-99 <62160075+adamaGueye-99@users.noreply.github.com>
2020-04-26 22:53:48 +0000

    Add files via upload

--- rectangle.java
+++ rectangle.java
@@ -1,57 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package application1;
 import java.util.Scanner;
- 
-class Rectangle {
- private static final Scanner scanner = new Scanner(System.in);
- 
- public static void main (String args[]) {
- double largeur = lireDonnee("largeur");
- double longueur = lireDonnee("longueur");
- 
- boolean donneesOk = testerDonnees(largeur,longueur);
- 
- if (donneesOk) {
- calculer(largeur, longueur);
- } else {
- afficherErreur();
- }
- }
- 
 
- static double lireDonnee (String text) {
- System.out.print("Entrez la " + text + ": ");
- return (scanner.nextDouble());
- }
- 
+/**
+ *
+ * @author hp
+ */
+public class rectangle {
 
- static void calculer(double l, double L) {
- System.out.print("Surface ('s/S') ou perimètre ('p/P')? ");
- char typeCalcul = scanner.next().charAt(0);
- switch (typeCalcul ){
- case 's':
- case 'S': {
- double surface = l * L;
- System.out.println("La surface est " + surface);
- break;
- }
- case 'p':
- case 'P': {
- double perimetre = 2 * (l + L);
- System.out.println("Le perimetre est est " + perimetre);
- break;
- }
- default: {
- System.out.println ("Introduire un 'p' ou un 's' s.v.p");
- }
- }
- }
- 
- static boolean testerDonnees(double largeur, double longueur) {
- return ((largeur > 0) && (longueur > 0));
- }
- 
-  static void afficherErreur () {
- System.out.println("Erreur: vous avez introduit une largeur " +
- "ou une longueur négative!");
- }
- 
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Veuillez saisir la largeur");
+        int l = sc.nextInt();
+        System.out.println("Veuillez saisir la longueur");
+        int L = sc.nextInt();
+        
+        System.out.println("1_perimetre");
+        System.out.println("2_surface");
+        System.out.println("3_demiperimetre");
+        System.out.println("4_quitter");
+        System.out.println("faites votre choix");
+        char ch = sc.next().charAt(0);
+        switch(ch){
+              case'1':
+              System.out.println(+((l+L))*2);
+                  break;
+              case'2':
+              System.out.println(+(l*L));
+                  break;
+              case'3':
+              System.out.println(+((l+L))/2);
+                  break;
+             
+              case'4':
+                  System.out.println("Aurevoir!!!!");
+                  break;
+                  
+        
+    }
+}
 }
