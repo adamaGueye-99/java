4db6c85d1d750eb53b7a73bb7fd2bc00d6ade259
adamaGueye-99 <62160075+adamaGueye-99@users.noreply.github.com>
2020-05-04 04:23:39 +0000

    Add files via upload

---
+++ carre.java
@@ -0,0 +1,21 @@
+public abstract class carre{
+    
+    private double cote;
+   
+
+    public carre( double cote) {
+        this.cote = cote;
+        
+    }
+
+    public double surface() {
+        return cote * cote;
+    }
+    public double perimetre() {
+        return (cote*4);
+   
+    public double getCote() {
+        return cote;
+    }
+
+   
